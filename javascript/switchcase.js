cf5e4f3762f03af4e4920a02c4486bffb0a0edcf
ThembakaziNgamlana <thembakazingamlana25@gmail.com>
2024-04-04 11:30:48 +0200

    adding switchcase.js file

---
+++ javascript/switchcase.js
@@ -0,0 +1,36 @@
+//Simple calculator using JS and switch case
+
+//we need this libary module to accept user input
+const prompt =require('prompt-sync')();
+
+let results;
+//accept the operator input 
+const operator= prompt(' enter operator (either +, -, *, or /)');
+
+//accept an operand input
+const num1= parseFloat(prompt('Enter the first number: '));
+const num2= parseFloat(prompt('Enter the second number: '));
+
+switch(operator){
+    //Formula for addition
+    case '+':
+    results = num1 + num2;
+    console.log(`${num1} + ${num2}=${results}`);
+    break;
+ //Formula for  substration
+    case '-':
+        results = num1 - num2;
+        console.log(`${num1} - ${num2}=${results}`);
+        break;  
+// Formula for multiplication
+        case '*':
+            results = num1 * num2;
+            console.log(`${num1} * ${num2}=${results}`);
+            break; 
+// Formula for division
+            case '/':
+                results = num1 / num2;
+                console.log(`${num1} / ${num2}=${results}`);
+                break; 
+}
+
--- javascript/variables.js
+++ javascript/variables.js
@@ -3,7 +3,6 @@ let times = 4;
 //block scope
 if(times <5){
    let greetings = " Hi there instead"
-   
 }
 console.log(greetings)
 //end scope
